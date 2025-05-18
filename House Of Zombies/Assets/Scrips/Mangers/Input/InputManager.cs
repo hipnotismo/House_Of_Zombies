47e55b1b52e29c69d97f1aed181878b6f53c9bdb
Martin <martinconcetti97@gmail.com>
2025-05-18 18:41:10 -0300

    Camera movement

--- House Of Zombies/Assets/Scrips/Mangers/Input/InputManager.cs
+++ House Of Zombies/Assets/Scrips/Mangers/Input/InputManager.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System;
 
 public class InputManager : MonoBehaviour
 {
     public delegate void PlayerMoveAction(Vector2 dir);
     public static event PlayerMoveAction MovePlayer;
 
+    //public delegate void CameraMoveAction(Vector2 dir);
+    //public static event CameraMoveAction MoveCamera;
+
+    public static Action<Vector2> MoveCamera;
     public void OnMove(InputValue inputValue)
     {
         Debug.Log("HERE!");
@@ -15,4 +20,10 @@ public class InputManager : MonoBehaviour
         MovePlayer(movementInput);
     }
 
+    public void OnLook(InputValue inputValue)
+    {
+
+        var cameraInput = inputValue.Get<Vector2>();
+        MoveCamera(cameraInput);
+    }
 }
---
+++ House Of Zombies/Assets/Scrips/Player/PlayerCamera.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCamera : MonoBehaviour
+{
+    [Header("Setup")]
+
+    [SerializeField] private Transform PlayerBody;
+
+    [SerializeField] private Transform CamHolder;
+    [SerializeField] private Transform CamWeaponLayer;
+
+    [Header("Movement")]
+
+    [SerializeField] private float MouseSensitivity = 100f;
+
+    private Vector2 MouseRot;
+
+    private float XRotation;
+
+    /// <summary>
+    /// Subscribes to input manager
+    /// </summary>
+    private void OnEnable()
+    {
+        InputManager.MoveCamera += CameraMovement;
+    }
+
+    /// <summary>
+    /// Updates teh mouse position and player rotation
+    /// </summary>
+    private void Update()
+    {
+        var mouseX = MouseRot.x * MouseSensitivity * Time.deltaTime;
+        var mouseY = MouseRot.y * MouseSensitivity * Time.deltaTime;
+
+        XRotation -= mouseY;
+        XRotation = Mathf.Clamp(XRotation, -70f, 70f);
+
+        CamHolder.localRotation = Quaternion.Euler(XRotation, 0f, 0f);
+        //CamWeaponLayer.localRotation = Quaternion.Euler(XRotation, 0f, 0f);
+
+        PlayerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    /// <summary>
+    /// Gets and actualices the camera vector every time the InputManager detects movement
+    /// </summary>
+    /// <param name="inputValue"></param>
+    public void CameraMovement(Vector2 inputValue)
+    {
+        MouseRot = inputValue;
+    }
+}
