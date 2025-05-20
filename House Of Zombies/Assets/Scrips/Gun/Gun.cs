d174385498775190721b7db346cb55cf8b690790
Martin <martinconcetti97@gmail.com>
2025-05-20 01:50:01 -0300

    Weapon trail

--- House Of Zombies/Assets/Scrips/Gun/Gun.cs
+++ House Of Zombies/Assets/Scrips/Gun/Gun.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gun : MonoBehaviour
 {
+    [SerializeField] TrailRenderer bulletTrail;
     private void OnEnable()
     {
         InputManager.Fire += Shoot;
@@ -21,6 +23,21 @@ public class Gun : MonoBehaviour
         Vector3 spawnPosition = transform.position;
         Vector3 spawnDirection = transform.forward;
 
+        if (Physics.Raycast(spawnPosition, spawnDirection, out hit, float.MaxValue))
+        {
+            TrailRenderer trail = Instantiate(bulletTrail, spawnPosition, Quaternion.identity);
+
+            StartCoroutine(SpawnTrail(trail, hit.point, hit.normal, true));
+
+        }
+        else
+        {
+            TrailRenderer trail = Instantiate(bulletTrail, spawnPosition, Quaternion.identity);
+
+            StartCoroutine(SpawnTrail(trail, spawnPosition + spawnDirection * 100, Vector3.zero, false));
+
+        }
+
         Physics.Raycast(spawnPosition, spawnDirection, out hit, 100f);
 
         ITakeDamage isHit = hit.collider.GetComponent<ITakeDamage>();
@@ -30,5 +47,26 @@ public class Gun : MonoBehaviour
             isHit.TakeDamage();
         }
 
+    }
+
+    private IEnumerator SpawnTrail(TrailRenderer Trail, Vector3 HitPoint, Vector3 HitNormal, bool MadeImpact)
+    {
+        
+        Vector3 startPosition = Trail.transform.position;
+        float distance = Vector3.Distance(Trail.transform.position, HitPoint);
+        float remainingDistance = distance;
+
+        while (remainingDistance > 0)
+        {
+            Trail.transform.position = Vector3.Lerp(startPosition, HitPoint, 1 - (remainingDistance / distance));
+
+            remainingDistance -= 100f * Time.deltaTime;
+
+            yield return null;
+        }
+        Trail.transform.position = HitPoint;
+        
+
+        Destroy(Trail.gameObject, Trail.time);
     }
 }
